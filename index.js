e85173a9c0d755503b86f92ceb21126398cf76a9
tkyg <80114433+tkyg@users.noreply.github.com>
2021-10-22 07:03:54 -0400

    commit tree 26409de9cf32c8c78853e674e84ff9fca35a7425

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 5
+Number.isInteger(random);
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let arr = [5, 10, 15, 20]
+let max = Math.max(...arr);
